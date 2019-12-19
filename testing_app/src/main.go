58d7d8e3311636856e991dacb79b4ba58373dd15
Nitesh Joshi <58712608+niteshfetch@users.noreply.github.com>
2019-12-18 17:03:09 -0800

    Updated main.go
    Wrote some code in main.go so that we can have an app which will print Hello World!

---
+++ testing_app/src/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func mainHandler() http.HandlerFunc {
+	return http.HandlerFunc(func (w http.REsponseWriter, r *http.REquest)  {
+		fmt.Fprintf(w, "Hello World! (Version info: %s, build date: %s)", os.Getenv("VERSION_INFO"), os.Getenv("BUILD_DATE"))
+	})
+	
+	func main()  {
+		http.HandlerFunc("/", mainHandler())
+		http.ListenAndServe(":8080", nil)
+	}
+}
